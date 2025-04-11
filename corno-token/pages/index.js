747401899d1ad0c6d55eadf03da3ae7e3f33b30b
DECARLISATAN <cornotoken@protonmail.com>
2025-04-10 22:31:10 -0300

    Adiciona página inicial

---
+++ corno-token/pages/index.js
@@ -0,0 +1,11 @@
+// pages/index.tsx
+
+export default function Home() {
+    return (
+      <div style={{ textAlign: 'center', marginTop: '3rem' }}>
+        <h1>Bem-vindo ao Corno Token</h1>
+        <p>O projeto mais fiel da blockchain!</p>
+      </div>
+    );
+  }
+  
